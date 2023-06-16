f209d265752c007bce6794c7f813956ea77d56b6
Theophilus Oyeyemi <105336834+MrPhilus@users.noreply.github.com>
2023-06-16 12:54:46 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let baseUrl = "https://api.adviceslip.com/advice",
+  adviceNum = document.getElementById("adviceNum"),
+  adviceBody = document.getElementById("adviceBody"),
+  randomAdvice = document.getElementById("randomAdvice");
+
+const generateAdvice = async () => {
+  try {
+    const response = await fetch(baseUrl);
+    let responseJson = await response.json();
+    adviceBody.innerText = `"${responseJson.slip.advice}"`;
+    adviceNum.innerText = `ADVICE #${responseJson.slip.id}`;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+randomAdvice.addEventListener("click", generateAdvice);
+window.addEventListener("load", generateAdvice);
